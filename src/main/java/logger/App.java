b49c07a6949be7871cb45b8fd9a1e5d4d4a464ec
muskmama-31 <saisabarishguntupalli@gmail.com>
2026-01-13 00:31:18 +0530

    Cleanup deployment configs

--- src/main/java/logger/App.java
+++ src/main/java/logger/App.java
@@ -16,16 +16,4 @@ public class App {
         System.out.println("Logger Instance: " + Logger.getInstance());
     }
 
-    @org.springframework.context.annotation.Bean
-    public org.springframework.web.servlet.config.annotation.WebMvcConfigurer corsConfigurer() {
-        return new org.springframework.web.servlet.config.annotation.WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
 }
